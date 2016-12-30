96db8c5110596b73f5b0837496585fa121055005
Priyank Verma <priyankvex95@gmail.com>
2016-12-30 20:22:55 +0530

    Add testing libraries. Espresso, Mockito and Junit$ Rules.

---
+++ app/src/test/java/com/wordpress/priyankvex/note_app_mvp/addnote/AddNotePresenterTest.java
@@ -0,0 +1,33 @@
+package com.wordpress.priyankvex.note_app_mvp.addnote;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by @priyankvex on 30/12/16.
+ */
+public class AddNotePresenterTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void onNewNoteClick() throws Exception {
+
+    }
+
+    @Test
+    public void saveNewNote() throws Exception {
+
+    }
+
+    @Test
+    public void setModel() throws Exception {
+
+    }
+
+}
